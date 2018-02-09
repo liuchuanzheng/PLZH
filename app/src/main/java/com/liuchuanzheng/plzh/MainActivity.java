2d446929a0ea22883f2eea765a3303dabb6509dd
liuchuanzheng <liuchuanzheng@feiliu.com>
2018-02-09 10:59:14 +0800

    完成

--- app/src/main/java/com/liuchuanzheng/plzh/MainActivity.java
+++ app/src/main/java/com/liuchuanzheng/plzh/MainActivity.java
@@ -6,7 +6,12 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,10 +25,12 @@ public class MainActivity extends AppCompatActivity {
     EditText etCount;
     @BindView(R.id.button)
     Button button;
-    @BindView(R.id.et_result)
-    EditText etResult;
     String content = "爱，萌，乐，优，福，鲜，吉，宝，麦，汪，旺，美，新，厨，生，龙，迪，中，品，果，米，多，悦";
     int count = 3;
+    @BindView(R.id.tv_result)
+    TextView tvResult;
+    private int all = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,51 +41,74 @@ public class MainActivity extends AppCompatActivity {
 
     private void initData() {
         etContent.setText(content);
-        etCount.setText(count+"");
+        etCount.setText(count + "");
     }
 
     @OnClick(R.id.button)
     public void onViewClicked() {
-        String finalContent= "";
+        all = 0;
+        tvResult.setText("");
+        String finalContent = "";
         int finalCount = 1;
         finalContent = etContent.getText().toString().trim();
         finalCount = Integer.valueOf(etCount.getText().toString().trim());
-        plzh(finalContent,finalCount);
+        plzh(finalContent, finalCount);
     }
 
-    private void plzh(String finalContent, int finalCount) {
-        if (TextUtils.isEmpty(finalContent)){
-            Toast.makeText(this,"要排列的内容为空",Toast.LENGTH_SHORT).show();
+    private void plzh(String finalContent, final int finalCount) {
+        if (TextUtils.isEmpty(finalContent)) {
+            Toast.makeText(this, "要排列的内容为空", Toast.LENGTH_SHORT).show();
             return;
         }
-        if (0 == finalCount){
-            Toast.makeText(this,"排列个数不对",Toast.LENGTH_SHORT).show();
+        if (0 == finalCount) {
+            Toast.makeText(this, "排列个数不对", Toast.LENGTH_SHORT).show();
             return;
         }
         String[] strings = finalContent.split("，");
-        for (String item:strings){
-            Log.i("liuchuanzheng",item);
-        }
-        if(finalCount > strings.length || strings.length <= 0){
-            return ;
-        }
 
-        String finalResult = "";
-        finalPLZH(finalResult,strings,finalCount);
+        for (String item : strings) {
+            Log.i("liuchuanzheng", item);
+        }
+        if (finalCount > strings.length || strings.length <= 0) {
+            return;
+        }
+        final List lst = Arrays.asList(strings);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                take("", finalCount, lst);
+            }
+        }).start();
+
 
     }
 
-    private String finalPLZH(String finalResult, String[] strings, int finalCount) {
-        if (finalCount == 1){
-            return finalResult;
-        }else{
-            for (int i=0;i<strings.length;i++){
-                for (int j=0;i<strings.length;i++){
+    public void take(String s, int total, final List lst) {
+        for (int i = 0; i < lst.size(); i++) {
+            //System.out.println("i="+i);
+            List templst = new LinkedList(lst);
+            String n = (String) templst.remove(i);// 取出来的数字
+            final String str = s + n;
+            if (total == 1) {
+                this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(str);//以最极端 n个里面只取一个，直接把取出来的结果输出即可
+                        //total=all;
+                        all++;
+                        System.out.println(all + "");//以最极端 n个里面只取一个，直接把取出来的结果输出即可
+                        tvResult.append(all+str+" " );
+                        if (all%6 == 0){
+                            tvResult.append("\n");
+                        }
+                    }
+                });
 
-                }
+            } else {
+                int temp = total - 1;//在同一层中total总量不能减,不能再原有变量的基础上
+                take(str, temp, templst);// 这里的temp以及templst都是全新的变量和集合
             }
         }
 
-        return finalResult;
     }
 }
